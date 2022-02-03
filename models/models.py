16df8e50fc473153b3f225807b0b289bd3e2f19d
Cesar <cesar250101@outlook.com>
2022-02-03 15:48:24 -0300

    Caclulo costo

--- models/models.py
+++ models/models.py
@@ -8,6 +8,16 @@ class Productos(models.Model):
 
     marca_id = fields.Many2one(comodel_name='method_minori.marcas', string='Marca')
     responsible_id = fields.Many2one(comodel_name='res.users',string='Responsable',related='marca_id.user_id')
+
+    @api.model
+    def calcular_costo(self):
+        marcar=self.env['method_minori.marcas'].search([])
+        for m in marcar:
+            productos_marca=self.env['product.template'].search([('marca_id','=',m.id)])
+            for p in productos_marca:
+                costo=round(p.list_price/1.19)*(1-(m.comision_marca/100))
+                p.standard_price=costo
+    
     
     
     
@@ -17,6 +27,8 @@ class Marcas(models.Model):
 
     name = fields.Char(string='Nombre Marca')
     user_id = fields.Many2one(comodel_name='res.users', string='Usuario')
+    comision_marca = fields.Integer(string='Comisión Marca')
+    
     
 
 

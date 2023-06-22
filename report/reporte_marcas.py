4e4a044f86810149bc3ea8a52aefd5adee210fba
Cesar250101 <Cesar250101@outlook.com>
2023-06-22 17:35:37 -0400

    Corrgige eporte de ventas por día

--- report/reporte_marcas.py
+++ report/reporte_marcas.py
@@ -20,26 +20,26 @@ class Ventas(models.Model):
         user=self.env.uid
         tools.drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""
-            CREATE OR REPLACE VIEW %s AS (
-                    ROW_NUMBER() OVER() AS id,
-                    date(po.date_order) as fecha_dia,
-                    EXTRACT(day FROM po.date_order) as dia,
-                    sum(pol.price_subtotal_incl) as total,
-                    sum(pol.price_subtotal_incl-pol.price_subtotal) as impuesto,
-                    sum(pol.price_subtotal) as neto,
-                    max(po.sii_document_number)  as Ultimo,min(po.sii_document_number) as Primero,
-                    mmm.es_propia 
-                    from pos_order po left join sii_document_class sdc on po.document_class_id =sdc.id
-                    inner join pos_order_line pol on po.id =pol.order_id 
-                    inner join product_product pp on pol.product_id =pp.id
-                    inner join product_template pt on pp.product_tmpl_id =pt.id  
-                    left join res_partner rp on po.partner_id =rp.id
-                    left join method_minori_marcas mmm on pt.marca_id =mmm.id
-                    left join product_category pc on pt.categ_id =pc.id 
-                    left join pos_session ps on po.session_id =ps.id 
-                    left join pos_config pc2 on ps.config_id =pc2.id
-                    group by date(po.date_order),EXTRACT(day FROM po.date_order),es_propia 
-                    order by date(po.date_order)
+            CREATE OR REPLACE VIEW %s AS (SELECT 
+                ROW_NUMBER() OVER() AS id,
+                date(po.date_order) as fecha_dia,
+                EXTRACT(day FROM po.date_order) as dia,
+                sum(pol.price_subtotal_incl) as total,
+                sum(pol.price_subtotal_incl-pol.price_subtotal) as impuesto,
+                sum(pol.price_subtotal) as neto,
+                max(po.sii_document_number)  as Ultimo,min(po.sii_document_number) as Primero,
+                mmm.es_propia 
+                from pos_order po left join sii_document_class sdc on po.document_class_id =sdc.id
+                inner join pos_order_line pol on po.id =pol.order_id 
+                inner join product_product pp on pol.product_id =pp.id
+                inner join product_template pt on pp.product_tmpl_id =pt.id  
+                left join res_partner rp on po.partner_id =rp.id
+                left join method_minori_marcas mmm on pt.marca_id =mmm.id
+                left join product_category pc on pt.categ_id =pc.id 
+                left join pos_session ps on po.session_id =ps.id 
+                left join pos_config pc2 on ps.config_id =pc2.id
+                group by date(po.date_order),EXTRACT(day FROM po.date_order),es_propia 
+                order by date(po.date_order)
             )
         """ % (
             self._table
